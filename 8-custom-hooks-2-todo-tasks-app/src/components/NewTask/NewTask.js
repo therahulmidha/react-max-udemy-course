d0a0b369e58e350d43126c33457eb027c9e4e32a
therahulmidha <rahul.midha31@gmail.com>
2021-12-09 08:51:45 +0530

    completed custom hooks and added form app

--- 8-custom-hooks-2-todo-tasks-app/src/components/NewTask/NewTask.js
+++ 8-custom-hooks-2-todo-tasks-app/src/components/NewTask/NewTask.js
@@ -1,41 +1,60 @@
-import { useState } from 'react';
+import useHttp from "../../hooks/use-http";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { error, isLoading, sendRequest } = useHttp();
+
+  const processData = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+    await sendRequest(
+      {
+        url: "https://react-max-course-4a914-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      processData.bind(null, taskText)
+      // ^ usage of method binding
+      // attaching an argument to the function being sent not called
+    );
+    // setIsLoading(true);
+    // setError(null);
+    // try {
+    //   const response = await fetch(
+    //     'https://react-max-course-4a914-default-rtdb.firebaseio.com/tasks.json',
+    //     {
+    //       method: 'POST',
+    //       body: JSON.stringify({ text: taskText }),
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     }
+    //   );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+    //   if (!response.ok) {
+    //     throw new Error('Request failed!');
+    //   }
 
-      const data = await response.json();
+    //   const data = await response.json();
 
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
+    // const generatedId = data.name; // firebase-specific => "name" contains generated id
+    // const createdTask = { id: generatedId, text: taskText };
 
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    // props.onAddTask(createdTask);
+    // } catch (err) {
+    //   setError(err.message || 'Something went wrong!');
+    // }
+    // setIsLoading(false);
   };
 
   return (
